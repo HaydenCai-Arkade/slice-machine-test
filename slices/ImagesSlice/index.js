f48051850e326802d47a6b9f1d47444f35e9fb06
HaydenCai-Arkade <internship@arkade.com.au>
2020-09-30 14:45:13 +1000

    added storybook

---
+++ slices/ImagesSlice/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { array, shape } from 'prop-types';
+import { RichText } from 'prismic-reactjs';
+
+const section = {
+  maxWidth: '600px',
+  margin: '4em auto',
+  textAlign: 'center',
+};
+
+const h2 = {
+  color: '#8592e0',
+};
+
+const MySlice = ({ slice }) => (
+  <section style={section}>
+    <h2 style={h2}>Template slice, update me!</h2>
+    { slice.primary.title ? <RichText render={slice.primary.title} /> : <p></p>}
+    { slice.primary.description ? <RichText render={slice.primary.description} /> : <p></p>}
+    { slice.primary.image ? <img src={slice.primary.image.url} alt=""/> : null}
+  </section>
+);
+
+MySlice.propTypes = {
+  slice: shape({
+    primary: shape({
+      title: array.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default MySlice;
---
+++ slices/index.js
@@ -0,0 +1,2 @@
+export { default as ImagesSlice } from './ImagesSlice'
+export { default as FormSlice } from './FormSlice'
